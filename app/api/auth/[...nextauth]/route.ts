4e2cd9e4afaf4431dab11e9f3d634c40810e49e9
mmuktar <9youngmoney@gmail.com>
2025-02-17 04:36:29 +0100

    removed redirect

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -14,7 +14,7 @@ const authOptions: NextAuthOptions = {
           scope: "openid profile user.Read email",
           response_type: "code",
           response_mode: "query",
-          redirect_uri: "https://elikita-test-apps.vercel.app" // Replace with your actual redirect URL
+         // redirect_uri: "https://elikita-test-apps.vercel.app" // Replace with your actual redirect URL
 
         },
       },
